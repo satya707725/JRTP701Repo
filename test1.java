7b223437842970165142abb9ac21eee1a5f9a349
satya707725 <dassatyanidhi@gmail.com>
2025-08-15 16:52:04 +0530

    dev2 modified the test1.java file

--- test1.java
+++ test1.java
@@ -2,4 +2,8 @@ class FirstProgram {
    public static void main(String[] args){
       System.out.println("First Program");
    }
+
+   public static void m1(){
+      System.out.println("M1 method");
+   }
 }
