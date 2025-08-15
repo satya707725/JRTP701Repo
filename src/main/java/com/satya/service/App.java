fb912788319737ba1c50dbdf1f296701bddc5157
satya707725 <dassatyanidhi@gmail.com>
2025-08-15 17:07:32 +0530

    dev1 changes the App.java file

--- src/main/java/com/satya/service/App.java
+++ src/main/java/com/satya/service/App.java
@@ -7,4 +7,8 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+
+    public static void m1(){
+        System.out.println("App m1 method");
+    }
 }
